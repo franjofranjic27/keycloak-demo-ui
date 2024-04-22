98ac2dfc0853219c65be77926a7bd6c94fd5b414
franjofranjic27 <franjic.f@bluewin.ch>
2024-04-22 12:38:45 +0200

    FRAF: Connecting to KeyCloak

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'keycloak-demo-ui' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('keycloak-demo-ui');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
